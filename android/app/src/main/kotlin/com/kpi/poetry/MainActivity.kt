e5936a3a11b3dcf1023e4f8531891e79b61092bd
ZayatsKateryna <58666983+SkrouFrost@users.noreply.github.com>
2020-10-29 23:18:12 +0200

    Add files via upload

---
+++ android/app/src/main/kotlin/com/kpi/poetry/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kpi.poetry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:poetry/Model/favorite_m.dart';
+import 'package:poetry/Model/history_m.dart';
+import 'package:poetry/main_page.dart';
+import 'package:poetry/favorite.dart';
+import 'package:poetry/history.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          Provider(create: (context) => HistoryModel()),
+          ChangeNotifierProxyProvider<HistoryModel, FavoriteModel>(
+            create: (context) => FavoriteModel(),
+            update: (context, history, favorite) {
+              favorite.history = history;
+              return favorite;
+            },
+          ),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Provider Demo',
+          initialRoute: '/',
+          routes: {
+            '/': (context) => MainPage(),
+            '/history': (context) => HistoryPage(),
+            '/favorite': (context) => FavoritePage(),
+          },
+          // home: MainPage()
+        ));
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(48),
+            child: AppBar(
+                titleSpacing: 0.0,
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.blueGrey[700],
+                title: MyTitle('Russian poetry  '))),
+        body: MainBody());
+  }
+}
+
+class HistoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(48),
+            child: AppBar(
+                titleSpacing: 0.0,
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.blueGrey[700],
+                title: MyTitle('History  '))),
+        body: HistoryBody());
+  }
+}
+
+class FavoritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(48),
+            child: AppBar(
+                titleSpacing: 0.0,
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.blueGrey[700],
+                title: MyTitle('Favorite  '))),
+        body: FavoriteBody());
+  }
+}
